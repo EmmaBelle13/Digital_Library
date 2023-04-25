ac58a60b1ff807e45c7f54c97eea9c9ce47b6661
EmmaBelle13 <horsleye24@hsnet.ahsd.org>
2023-04-25 10:34:48 -0400

    4/25/2023 Added some colors and began the process of setting up the recycler view

--- app/src/main/java/com/example/digitallibrary/Book.kt
+++ app/src/main/java/com/example/digitallibrary/Book.kt
@@ -1,4 +1,4 @@
 package com.example.digitallibrary
 
- data class Book (var title: String, var author: String, var color: String, var bookHeight: Int, var bookWidth: Int) {
+ data class Book (var title: String, var author: String, var color: String, var bookHeight: Int, var summary: String) {
 }
